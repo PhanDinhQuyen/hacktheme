574331a855a86016f7298a1bea39c80dbcad9021
LouisPhan <phanquyen7799@gmail.com>
2021-12-27 00:48:41 +0700

    Add new file hack theme

---
+++ app.js
@@ -0,0 +1,74 @@
+console.log("%cHello chồng iu", "font-size: 50px; ");
+console.log("%c<3", "color: red; font-size: 100px; font-weight: bold;");
+//Create Canvas
+const canvas = document.createElement("canvas");
+canvas.setAttribute("id", "canvas");
+//Add canvas to body
+document.body.appendChild(canvas);
+
+const ctx = canvas.getContext("2d");
+
+let cw = this.innerWidth;
+let ch = this.innerHeight;
+//Get data from file json then push to this array
+const charArr = [];
+//Get data
+(async function getData() {
+  const data = await fetch("data/data.json").then((res) => res.json());
+  data.dataChar.map((e) => charArr.push(e));
+})();
+let maxCharCount = 500;
+
+const fallingCharArr = [];
+
+let fontSize = 14;
+// Somethings....
+let maxColumns = cw / fontSize;
+
+let frames = 0;
+canvas.width = cw;
+canvas.height = ch;
+//Create class
+
+class FallingChar {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw(ctx) {
+    this.value = charArr[Math.floor(Math.random() * (charArr.length - 1))];
+    this.speed = (Math.random() * fontSize * 3) / 4 + (fontSize * 3) / 4;
+
+    ctx.fillStyle = "rgba(0,255,0)";
+    ctx.font = fontSize + "px san-serif";
+    ctx.fillText(this.value, this.x, this.y);
+    this.y += this.speed;
+
+    if (this.y > ch) {
+      this.y = (Math.random() * ch) / 2 - 50;
+      this.x = Math.floor(Math.random() * maxColumns) * fontSize;
+      this.speed = (-Math.random() * fontSize * 3) / 4 + (fontSize * 3) / 4;
+    }
+  }
+}
+
+let update = () => {
+  if (fallingCharArr.length < maxCharCount) {
+    let fallingChar = new FallingChar(
+      Math.floor(Math.random() * maxColumns) * fontSize,
+      (Math.random() * ch) / 2 - 50
+    );
+    fallingCharArr.push(fallingChar);
+  }
+  ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+  ctx.fillRect(0, 0, cw, ch);
+  for (let i = 0; i < fallingCharArr.length && frames % 2 == 0; i++) {
+    fallingCharArr[i].draw(ctx);
+  }
+
+  requestAnimationFrame(update);
+  frames++;
+};
+
+update();
